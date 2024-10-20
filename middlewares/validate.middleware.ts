c6fa41b0087506d23c89a2bded499e41fbf3dfca
Daniel Rojas <notdanielrojas@gmail.com>
2024-10-20 18:44:18 -0300

    backend revisited

--- middlewares/validate.middleware.ts
+++ middlewares/validate.middleware.ts
@@ -23,9 +23,9 @@ const validateCredentialsAtLogin = (req: Request, res: Response, next: NextFunct
 };
 
 const validateCredentialsAtSubmit = (req: Request, res: Response, next: NextFunction): void => {
-  const { image_url, title, description, user } = req.body;
+  const { image_url, title, description } = req.body;
 
-  if (!image_url || !title || !description || !user?.id) {
+  if (!image_url || !title || !description) {
     res.status(400).json({ message: "All fields are required" });
     return;
   }
