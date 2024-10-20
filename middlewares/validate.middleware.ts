1bcbb943da9d91f7cd8581417252a1acb352b72c
Daniel Rojas <notdanielrojas@gmail.com>
2024-10-20 18:57:29 -0300

    testing put

--- middlewares/validate.middleware.ts
+++ middlewares/validate.middleware.ts
@@ -23,10 +23,10 @@ const validateCredentialsAtLogin = (req: Request, res: Response, next: NextFunct
 };
 
 const validateCredentialsAtSubmit = (req: Request, res: Response, next: NextFunction): void => {
-  const { image_url, title, description } = req.body;
+  const { image_url, title, description, user } = req.body;
 
-  if (!image_url || !title || !description) {
-    res.status(400).json({ message: "All fields are required" });
+  if (!image_url || !title || !description || !user?.id) {
+    res.status(400).json({ message: "All fields are required, including user ID" });
     return;
   }
 
